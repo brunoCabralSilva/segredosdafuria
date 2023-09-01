e48f1fb0eed861d7454e990f25465bfadadb5104
bruno <bruno.cabral.silva2018@gmail.com>
2023-09-01 15:07:01 -0300

    Criando todas as rotas e adicionando componente 'Em Construção'

---
+++ src/app/trybes/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import Nav from '@/components/nav';
+import Footer from '@/components/footer';
+import InConstruction from '@/components/inConstruction';
+
+export default function Trybes() {
+  return (
+    <div className="w-full h-screen bg-ritual bg-cover bg-top relative">
+      <div className="absolute bg-black/80 w-full h-full" />
+      <Nav />
+      <InConstruction />
+      <Footer />
+    </div>
+  );
+}
---
+++ src/components/inConstruction.tsx
@@ -0,0 +1,53 @@
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+export default function InConstruction() {
+  return(
+    <section className="relative h-full flex flex-col items-center justify-center text-white text-2xl font-andika">
+      <motion.h2
+        initial={{opacity:0}}
+        animate={{opacity:1, transition: {delay: 0.2, duration: 1 }}}
+        exit={{opacity:0, transition: { duration: 0.5 }}}
+        className="mb-6 text-center w-10/12 mt-20 sm:mt-0"
+      >
+        Estamos em Construção...
+      </motion.h2>
+      <motion.h2
+        initial={{opacity:0}}
+        animate={{opacity:1, transition: {delay: 0.3, duration: 1 }}}
+        exit={{opacity:0, transition: { duration: 0.5 }}}
+        className="text-center w-10/12"
+      >
+        Aguarde e em breve retornaremos mais fortes!
+      </motion.h2>
+      <motion.div 
+        initial={{opacity:0}}
+        animate={{opacity:1, transition: {delay: 0.3, duration: 1 }}}
+        exit={{opacity:0, transition: { duration: 0.5 }}}
+        className="flex flex-row justify-center"
+      >
+        <Image
+          src="/images/logos/wolf_run.gif"
+          alt="gif de lobo branco correndo"
+          className="constr-gif-wolf"
+          width={200}
+          height={100}
+        />
+        <Image
+          src="/images/logos/wolf_run.gif"
+          alt="gif de lobo branco correndo"
+          className="hidden sm:flex constr-gif-wolf"
+          width={200}
+          height={100}
+        />
+        <Image
+          src="/images/logos/wolf_run.gif"
+          alt="gif de lobo branco correndo"
+          className="hidden md:flex an constr-gif-wolf"
+          width={200}
+          height={100}
+        />
+      </motion.div>
+    </section>
+  );
+}
