5e1d909ba08c822e09a5a355fd2621aa812f5ef1
bruno <bruno.cabral.silva2018@gmail.com>
2023-09-26 01:21:24 -0300

    Correção de tamanho de fonte no modo mobile

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -37,7 +37,7 @@ export default function Content(props: { object: IContent }) {
   };
 
   return(
-    <div className="py-10 flex flex-col items-center sm:items-start w-full z-20 text-white px-8 text-justify">
+    <div className="py-10 flex flex-col items-center sm:items-start w-full z-20 text-white text-justify overflow-y-auto">
       <div className="flex items-center justify-center w-full relative h-full">
         <div className="absolute h-full w-full sm:w-5/12 flex items-center justify-center">
           { isLoading && <span className="loader z-50" /> }
@@ -45,78 +45,80 @@ export default function Content(props: { object: IContent }) {
         <Image
           src={`/images/trybes/${object.namePtBr} - representation.png`}
           alt={`Representação dos ${object.namePtBr}`}
-          className="w-full sm:w-5/12 relative"
+          className="w-full mobile:w-8/12 sm:w-5/12 relative px-4"
           width={800}
           height={400}
           onLoad={() => setIsLoading(false)} // Define isLoading como falso quando a imagem é carregada
         />
       </div>
-      <h2 className="mt-10 font-bold text-2xl w-full text-center">
-        {`${object.namePtBr} (${firstLetter(object.nameEn)})`}
-      </h2>
-      <div className="flex items-center justify-center w-full">
-        <p className="mt-3 mb-10 text-center sm:w-1/2">
-          &quot;{ object.phrase }&quot;
-        </p>
-      </div>
-      {
-        object.description.map((paragraph: String, index: number) => (
-          <p key={ index }>
-            { paragraph }
+      <div className="mt-5 mobile:mt-10 px-6 text-sm sm:text-base">
+        <h2 className=" font-bold text-xl sm:text-2xl w-full text-center">
+          {`${object.namePtBr} (${firstLetter(object.nameEn)})`}
+        </h2>
+        <div className="flex items-center justify-center w-full text-sm">
+          <p className="mt-1 sm:mt-3 mb-10 text-center sm:w-1/2">
+            &quot;{ object.phrase }&quot;
           </p>
-        ))
-      }
-      <div className="flex items-center justify-center w-full">
-        <Image
-          src={`/images/trybes/${object.namePtBr}.png`}
-          alt={`Glifo dos ${object.namePtBr}`}
-          className="w-20 sm:w-38"
-          width={800}
-          height={400}
-        />
-      </div>
-      <h2 className="text-center sm:text-left w-full text-xl">
-        Quem são
-      </h2>
-      <hr className="w-10/12 my-2" />
-      {
-        object.whoAre.map((paragraph: String, index: number) => (
-          <p key={ index }>
-            { paragraph }
-          </p>
-        ))
-      }
-      
-      <h2 className="text-center sm:text-left w-full pt-8 text-xl">
-        Patrono Espiritual
-      </h2>
-      <hr className="w-10/12 my-2" />
-      <p>{object.patron}</p>
-      
-      <div>
-        <span className="underline">Favor</span>
-        <span className="px-1">-</span>
-        <span>{object.favor}</span>
-      </div>
-      <div>
-        <span className="underline">Proibição</span>
-        <span className="px-1">-</span>
-        <span>{object.ban}</span>
-      </div>
-      
-      <h2 className="text-center sm:text-left w-full pt-8 text-xl">
-        Arquétipos
-      </h2>
-      <hr className="w-10/12 mt-2 mb-5" />
-      <div>
+        </div>
         {
-          object.archetypes.map((archetype: IArchetypes, index: number) => (
-            <div key={ index }>
-              <h2 className="underline">{ archetype.title } </h2>
-              <p className="pb-5">{ archetype.description }</p>
-            </div>
+          object.description.map((paragraph: String, index: number) => (
+            <p key={ index }>
+              { paragraph }
+            </p>
           ))
         }
+        <div className="flex items-center justify-center w-full">
+          <Image
+            src={`/images/trybes/${object.namePtBr}.png`}
+            alt={`Glifo dos ${object.namePtBr}`}
+            className="w-20 sm:w-38"
+            width={800}
+            height={400}
+          />
+        </div>
+        <h2 className="text-center sm:text-left w-full text-xl">
+          Quem são
+        </h2>
+        <hr className="w-10/12 my-2" />
+        {
+          object.whoAre.map((paragraph: String, index: number) => (
+            <p key={ index }>
+              { paragraph }
+            </p>
+          ))
+        }
+        
+        <h2 className="text-center sm:text-left w-full pt-8 text-xl">
+          Patrono Espiritual
+        </h2>
+        <hr className="w-10/12 my-2" />
+        <p>{object.patron}</p>
+        
+        <div>
+          <span className="underline">Favor</span>
+          <span className="px-1">-</span>
+          <span>{object.favor}</span>
+        </div>
+        <div>
+          <span className="underline">Proibição</span>
+          <span className="px-1">-</span>
+          <span>{object.ban}</span>
+        </div>
+        
+        <h2 className="text-center sm:text-left w-full pt-8 text-xl">
+          Arquétipos
+        </h2>
+        <hr className="w-10/12 mt-2 mb-5" />
+        <div>
+          {
+            object.archetypes.map((archetype: IArchetypes, index: number) => (
+              <div key={ index }>
+                <h2 className="underline">{ archetype.title } </h2>
+                <p className="pb-5">{ archetype.description }</p>
+              </div>
+            ))
+          }
+        </div>
     </div>
    </div>
   );
