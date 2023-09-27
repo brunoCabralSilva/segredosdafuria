404218f74a5616ebfaf336ff3729082d2451faf3
bruno <bruno.cabral.silva2018@gmail.com>
2023-09-27 19:22:05 -0300

    exibição de dados em 'loresheets' sem estilização

--- src/app/loresheets/page.tsx
+++ src/app/loresheets/page.tsx
@@ -4,7 +4,20 @@ import Footer from '@/components/footer';
 import { useSlice } from '@/redux/slice';
 import { useAppSelector } from '@/redux/hooks';
 import Simplify from '@/components/simplify';
-import InConstruction from '@/components/inConstruction';
+import listLoresheets from '../../data/loresheets.json';
+
+interface IHabilities {
+  skill: String;
+  skillPtBr: String;
+}
+
+interface ILoresheet {
+  titlePtBr: String;
+  title: String;
+  descriptionPtBr: String;
+  description: String;
+  habilities: IHabilities[];
+}
 
 export default function Loresheets() {
   const slice = useAppSelector(useSlice);
@@ -14,9 +27,27 @@ export default function Loresheets() {
       <div className="px-2 z-10 relative">
         <Simplify />
         <Nav />
-        <InConstruction />
-        <Footer />
+        <div>
+          {
+            listLoresheets.map((loresheet: ILoresheet, index: number) => (
+              <div key={ index } className="py-10">
+                <p>{ loresheet.titlePtBr } - { loresheet.title }</p>
+                <p>{ loresheet.descriptionPtBr }</p>
+                <p>{ loresheet.description }</p>
+                {
+                  loresheet.habilities.map((hability: IHabilities, index: number) => (
+                    <div key={ index }>
+                      <p>{ hability.skill }</p>
+                      <p>{ hability.skillPtBr }</p>
+                    </div>
+                  ))
+                }
+              </div>
+            ))
+          }
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
