ad82b8d2eef9b11201b19f99ac6ca45cc944a5a9
bruno <bruno.cabral.silva2018@gmail.com>
2023-11-22 21:13:31 -0300

    Conclusão de chat

--- src/app/sessions/page.jsx
+++ src/app/sessions/page.jsx
@@ -6,182 +6,81 @@ import { useState } from 'react';
 
 export default function Chat() {
   const [text, setText] = useState('');
+  const db = getFirestore(firestoreConfig);
+  const messageRef = collection(db, "chatbot");
+  const queryMessages = query(messageRef, orderBy("date"), limit(25));
+  const [messages] = useCollectionData(queryMessages, { idField: "id" });
+  const [showData, setShowData] = useState(true);
 
-  const ChatRoom = () => {
-    const db = getFirestore(firestoreConfig);
-    const messageRef = collection(db, "chatbot");
-    const queryMessages = query(messageRef, orderBy("date"), limit(25));
-    const [messages] = useCollectionData(queryMessages, { idField: "id" });
+  const typeText = (e) => {
+    const sanitizedValue = e.target.value.replace(/\s+/g, ' ');
+    setText(sanitizedValue);
+  };
 
-    const sendMessage = async (e) => {
-      e.preventDefault();
+  const sendMessage = async () => {
+    if (text.trim() !== '') {
       await addDoc(
         messageRef,
         {
           message: text,
-          user:"bruno",
+          user: "bruno",
           date: serverTimestamp()
         }
       );
       setText('');
-    };
+      scrollToBottom();
+    }
+  };
 
-    return (
-      <div>
-        <div>
-          {messages && messages.map((msg, index) => <ChatMessage key={index} message={ msg }/>)}
+  const scrollToBottom = () => {
+    const messagesContainer = document.getElementById('messages-container');
+    if (messagesContainer) {
+      messagesContainer.scrollTop = messagesContainer.scrollHeight;
+    }
+  };
+
+  return (
+    showData && (
+      <div className="h-screen overflow-y-auto">
+        <div id="messages-container" className="h-90vh overflow-y-auto p-2">
+          {messages && messages.map((msg, index) => (
+            <div key={index} className="p-2 bg-gray-500 my-2">
+              <div>
+                {msg.user} {msg.date && msg.date.toDate().toLocaleString()}{' '}
+              </div>
+              <div>{msg.message}</div>
+            </div>
+          ))}
         </div>
-        <form onSubmit={ sendMessage }>
-          <input type="text" value={text} onChange={ (e) => setText(e.target.value) } />
-          <button type="submit">Enviar</button>
-        </form>
+        <div className="fixed bottom-0 w-full bg-black p-2 flex gap-3">
+          <input
+            type="text"
+            className="w-9/12 p-2"
+            value={text}
+            onChange={(e) => typeText(e)}
+          />
+          <div className="grid grid-cols-4 w-3/12 gap-3">
+            <button
+              className="border border-white text-white h-full p-2"
+              onClick={sendMessage}
+            >
+              Enviar
+            </button>
+            <button className="border border-white text-white h-full p-2">
+              Dice
+            </button>
+            <button className="border border-white text-white h-full p-2">
+              Ficha
+            </button>
+            <button
+              className="border border-white text-white h-full p-2"
+              onClick={ scrollToBottom }
+            >
+              Fim
+            </button>
+          </div>
+        </div>
       </div>
-    );
-  };
-
-  const ChatMessage = (props) => {
-    const { text, date, message,  } = props.message;
-
-    return(
-      <div>
-        { message && <div>{ message }</div> }
-      </div>
-    );
-
-  };
-
-  return(
-    <div>
-      <ChatRoom />
-    </div>
+    )
   );
 }
-
-
-// import { useRouter } from 'next/navigation';
-// import { useState, useEffect } from 'react';
-// import { verify } from '../../firebase/user';
-
-// import {
-//   collection,
-//   onSnapshot,
-//   addDoc,
-//   getDocs,
-//   query,
-//   orderBy,
-//   serverTimestamp,
-// } from 'firebase/firestore/lite'; // Importações específicas para o Firestore Lite
-
-// import { getFirestore } from 'firebase/firestore/lite';
-// import firebaseConfig from '@/firebase/connection';
-
-// export default function Sessions() {
-//   const router = useRouter();
-//   const [showData, setShowData] = useState(false);
-//   const [message, setMessage] = useState('');
-//   const [messages, setMessages] = useState([]);
-
-//   const db = getFirestore(firebaseConfig);
-//   const chatbotRef = collection(db, 'chatbot');
-//   const chatbotQuery = query(chatbotRef, orderBy('date', 'asc'));
-
-//   useEffect(() => {
-//     setShowData(false);
-//     window.scrollTo(0, 0);
-//     const token = localStorage.getItem('Segredos Da Fúria');
-//     if (token) {
-//       const decodedToken = verify(JSON.parse(token));
-//       if (!decodedToken) {
-//         router.push('/sessions/login');
-//       } else {
-//         setShowData(true);
-//       }
-//     } else {
-//       router.push('/sessions/login');
-//     }
-//   }, [router]);
-
-//   useEffect(() => {
-//     const chatBot = async () => {
-//       const querySnapshot = await getDocs(chatbotRef);
-//       const obj = [];
-//       querySnapshot.forEach((doc) => {
-//         obj.push({
-//           id: doc.id,
-//           message: doc.data().message,
-//           user: doc.data().user,
-//           date: doc.data().date,
-//         });
-//       });
-
-//       setMessages(obj);
-//     };
-
-//     chatBot();
-//   }, [chatbotRef]);
-
-//   useEffect(() => {
-//     // Adiciona um listener para atualizações em tempo real
-//     const unsubscribe = onSnapshot(chatbotQuery, (snapshot) => {
-//       const updatedMessages = snapshot.docs.map((doc) => ({
-//         id: doc.id,
-//         ...doc.data(),
-//       }));
-//       setMessages(updatedMessages);
-//     });
-
-//     // Limpa o listener quando o componente é desmontado
-//     return () => unsubscribe();
-//   }, [chatbotQuery]);
-
-//   const sendMessage = async () => {
-//     const timestamp = serverTimestamp();
-
-//     await addDoc(chatbotRef, {
-//       message: message,
-//       user: 'bruno',
-//       date: timestamp,
-//     });
-
-//     setMessage('');
-//   };
-
-//   return (
-//     showData && (
-//       <div className="h-screen overflow-y-auto">
-//         <div className="h-90vh overflow-y-auto p-2">
-//           {messages.map((msg, index) => (
-//             <div key={index} className="p-2 bg-gray-500 my-2">
-//               <div>
-//                 {msg.user} {msg.date && msg.date.toDate().toLocaleString()}{' '}
-//               </div>
-//               <div>{msg.message}</div>
-//             </div>
-//           ))}
-//         </div>
-//         <div className="fixed bottom-0 w-full bg-black p-2 flex gap-3">
-//           <input
-//             type="text"
-//             className="w-9/12 p-2"
-//             value={message}
-//             onChange={(e) => setMessage(e.target.value)}
-//           />
-//           <div className="grid grid-cols-3 w-3/12 gap-3">
-//             <button
-//               className="border border-white text-white h-full p-2"
-//               onClick={sendMessage}
-//             >
-//               Enviar
-//             </button>
-//             <button className="border border-white text-white h-full p-2">
-//               Dice
-//             </button>
-//             <button className="border border-white text-white h-full p-2">
-//               Ficha
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     )
-//   );
-// }
