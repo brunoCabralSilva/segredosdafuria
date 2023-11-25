e80d9edda88e939ae196be09a962917a48679969
bruno <bruno.cabral.silva2018@gmail.com>
2023-11-24 21:25:19 -0300

    Correção de cores para generals

--- src/components/sheet/general.tsx
+++ src/components/sheet/general.tsx
@@ -1,7 +1,7 @@
 export default function General() {
   return(
     <div className="flex flex-col w-full overflow-y-auto pr-2 h-full mb-1">
-      <div className="w-full h-full mb-2 p-1 cursor-pointer text-white bg-black flex-col items-start justify-center font-bold">
+      <div className="w-full h-full mb-2 p-1 cursor-pointer text-black bg-white flex-col items-start justify-center font-bold px-4">
         <div className="w-full flex-col items-center justify-start mt-1">
           <div className="my-5">
             <span className="break-words text-xl">
@@ -28,31 +28,31 @@ export default function General() {
             <div className="pr-5">
               <span>Honra:</span>
               <div className="flex gap-1">
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
               </div>
             </div>
             <div className="pr-5">
               <span>Glória:</span>
               <div className="flex gap-1">
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
               </div>
             </div>
             <div className="pr-5">
               <span>Sabedoria:</span>
               <div className="flex gap-1">
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
               </div>
             </div>
           </div>
@@ -62,21 +62,21 @@ export default function General() {
             <span>Vitalidade:</span>
             <div className="flex">
               <div className="flex flex-wrap gap-1 pt-1">
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-                <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+                <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
               </div>
             </div>
           </div>
@@ -84,24 +84,24 @@ export default function General() {
         <div className="w-full mb-2 mt-4 text-blackflex-col items-center justify-start font-bold">
           <span>Fúria:</span>
           <div className="flex gap-2 pt-1">
-            <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-            <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-            <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-            <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-            <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
+            <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+            <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+            <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+            <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+            <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
           </div>
         </div>
         <div className="w-full mt-4 text-blackflex-col items-center justify-start font-bold">
           <span>Força de Vontade:</span>
           <div className="flex flex-wrap gap-2 pt-1">
-            <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-            <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-            <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-            <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-            <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-            <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-            <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
-            <div className="h-5 w-5 bg-black border-2 border-white cursor-pointer" />
+            <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+            <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+            <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+            <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+            <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+            <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+            <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
+            <div className="h-5 w-5 bg-white border-2 border-black cursor-pointer" />
           </div>
         </div>          
       </div>
