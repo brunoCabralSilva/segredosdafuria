d040be810e376702c5945a6b7a7c9d58a25cbc08
Bruno <bruno.cabral.silva2018@gmail.com>
2025-03-06 22:42:47 -0300

    mudança no menu de usuário

--- src/components/sessionBar.tsx
+++ src/components/sessionBar.tsx
@@ -1,14 +1,16 @@
 'use client'
 import { IoIosSend } from "react-icons/io";
 import { FaEraser } from "react-icons/fa";
-import { FaPlus } from "react-icons/fa6";
+import { FaChevronDown, FaFile, FaPlus } from "react-icons/fa6";
 import { useContext, useState } from 'react';
 import contexto from '@/context/context';
 import { registerMessage } from "../firebase/messagesAndRolls";
 import { GiD10 } from "react-icons/gi";
+import { CiFileOn } from "react-icons/ci";
 
 export default function SessionBar() {
   const [text, setText] = useState('');
+  const [menu, setMenu] = useState(false);
   const {
     sessionId,
     session,
@@ -21,6 +23,50 @@ export default function SessionBar() {
 
   return(
     <div className={`${showMenuSession !== '' ? 'absolute' : 'fixed'} bottom-0 w-full bg-black p-2 flex flex-col gap-2 justify-center items-center min-h-10vh`}>
+      {
+        menu &&
+        <div className="bg-black flex w-full justify-end gap-2">
+          <div className="text-xl border border-white flex justify-center hover:bg-white transition-colors text-white hover:text-black">
+            <button
+              className="p-2"
+              title="Realizar um teste com dados"
+              onClick={() => {
+                setShowMenuSession('dices');
+                scrollToBottom();
+              }}
+            >
+              <GiD10 />
+            </button>
+          </div>
+          <div className="text-xl border border-white flex justify-center hover:bg-white transition-colors text-white hover:text-black">
+            <button
+              className="p-2"
+              title="Acessar o Menu da Sessão"
+              onClick={() => {
+                setShowMenuSession('sheet');
+                scrollToBottom();
+              }}
+            >
+              <FaFile className="" />
+            </button>
+          </div>
+          { 
+            session.gameMaster === email &&
+            <div className="text-xl border border-white flex justify-center hover:bg-white transition-colors text-white hover:text-black">
+              <button
+                className="p-2"
+                title="Apagar o histórico de conversas"
+                onClick={ async () => {
+                  setShowDeleteHistoric(true);
+                  scrollToBottom();
+                }}
+              >
+                <FaEraser />
+              </button>
+            </div>
+          }
+        </div>
+      }
       <div className="flex w-full items-end relative">
         <textarea
           rows={Math.max(1, Math.ceil(text.length / 40))}
@@ -54,18 +100,6 @@ export default function SessionBar() {
               <IoIosSend />
             </button>
           </div>
-          <div className="text-xl border border-white flex justify-center hover:bg-white transition-colors text-white hover:text-black">
-            <button
-              className="p-2"
-              title="Realizar um teste com dados"
-              onClick={() => {
-                setShowMenuSession('dices');
-                scrollToBottom();
-              }}
-            >
-              <GiD10 />
-            </button>
-          </div>
           {
             showMenuSession === '' &&
             <div className="text-xl border border-white flex justify-center hover:bg-white transition-colors text-white hover:text-black">
@@ -73,29 +107,18 @@ export default function SessionBar() {
                 className="p-2"
                 title="Acessar o Menu da Sessão"
                 onClick={() => {
-                  setShowMenuSession('sheet');
+                  setMenu(!menu);
                   scrollToBottom();
                 }}
               >
-                <FaPlus />
+                {
+                  menu
+                  ? <FaChevronDown />
+                  : <FaPlus />
+                }
               </button>
             </div>
           }
-          { 
-            session.gameMaster === email &&
-              <div className="text-xl border border-white flex justify-center hover:bg-white transition-colors text-white hover:text-black">
-                <button
-                  className="p-2"
-                  title="Apagar o histórico de conversas"
-                  onClick={ async () => {
-                    setShowDeleteHistoric(true);
-                    scrollToBottom();
-                  }}
-                >
-                  <FaEraser />
-                </button>
-              </div>
-            }
         </div>
       </div>
     </div>
