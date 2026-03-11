1892a11243995ef95d0ecb5bcfe436f859344d8c
Bruno <bruno.cabral.silva2018@gmail.com>
2026-03-10 23:58:40 -0300

    Correção de imagem de dados, não existem brutais nos dados comuns

--- src/components/dicesAndMessages/dice.tsx
+++ src/components/dicesAndMessages/dice.tsx
@@ -5,12 +5,10 @@ export default function Dice(props: { dice: number, type: string }) {
   let imgItem = '';
   if (Number(dice) === 10) {
     imgItem = `critical${type}.png`;
-  } else if (Number(dice) > 2 && Number(dice) < 6) {
-    imgItem = `falha${type}.png`;
   } else if (Number(dice) > 5 && Number(dice) < 10) {
     imgItem = `success${type}.png`;
   } else {
-    imgItem = `brutal${type}.png`;
+    imgItem = `falha${type}.png`;
   }
   return (
     <div className="flex flex-col items-center justify-center">
